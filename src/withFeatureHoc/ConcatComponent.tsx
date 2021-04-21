03760b70298e2e28ccf75c65d74f092f83201254
Iván Reinoso García <ireinoso@plainconcepts.com>
2021-04-22 00:02:51 +0200

    another performance improvement

--- src/withFeatureHoc/ConcatComponent.tsx
+++ src/withFeatureHoc/ConcatComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   text: string;
@@ -25,4 +25,6 @@ const ConcatComponent = (props: Props) => {
   );
 };
 
-export { ConcatComponent };
+const ConcatComponentMemo = React.memo(ConcatComponent);
+
+export { ConcatComponentMemo as ConcatComponent };
